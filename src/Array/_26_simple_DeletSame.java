57043fc0f8c887054107b24eb8fdac7bf65fe2ef
HillkingF <976129707@qq.com>
2020-07-19 16:50:41 +0800

    Update _26_simple_DeletSame.java

--- src/Array/_26_simple_DeletSame.java
+++ src/Array/_26_simple_DeletSame.java
@@ -12,12 +12,12 @@ public class _26_simple_DeletSame {
      * 不使用额外的数组空间，原地修改数组
      * 创造两个指针i,j
      * 初始：i,j都在0位置
-     * 首先依次+1移动j，将j位置的元素与i位置比较，如果一样j+1,如果j不一样，将此时j开始的元素都往前移到i+1开始的位置
-     * 令i等于j,重复上述操作
+     * 首先依次+1移动j，将j位置的元素与i位置比较，如果一样j+1,如果j不一样，令nums[j]赋值到i下一个位置
+     * i = i+1
      */
     public static void main(String[] args){
         Solution sobj = new Solution();
-        int[] nums = {1,2};
+        int[] nums = {1,1,2};
         int[] nums1 = {};
         int[] nums2 = {0,0,0,1,1,2,3};
         int i = sobj.removeDuplicates(nums);
@@ -29,14 +29,24 @@ public class _26_simple_DeletSame {
 class Solution{
     public int removeDuplicates(int[] nums) {
         int i = 0;
-        int j = 0;
         if (nums.length < 2){
             return nums.length;
         }
-        for (j=1; j<nums.length; j++){
-
-
+        for(int num: nums){
+            if (num != nums[i]){
+                nums[i+1] = num;
+                i = i+1;
+            }
         }
+        return i+1;
+
+//        for (int j=1; j<nums.length; j++){
+//            if(nums[j] != nums[i]){
+//                nums[i + 1] = nums[j];
+//                i += 1;
+//            }
+//        }
+//        return i + 1;
 
 
 
