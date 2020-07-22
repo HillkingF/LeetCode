b05ced4352855478d31b1783a60aaaf4ed0b4a2c
HillkingF <976129707@qq.com>
2020-07-22 09:17:14 +0800

    Array-T35

--- src/Array/_27_simple_delyuansu.java
+++ src/Array/_27_simple_delyuansu.java
@@ -6,7 +6,7 @@ package Array;
  * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
  * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
  */
-public class _27_delyuansu {
+class _27_delyuansu {
     public static void main(String[] args){
         Solution27 s = new Solution27();
         int[] nums = {3,2,3,3};
--- src/Array/_35_simple_ForkInsertLocation.java
+++ src/Array/_35_simple_ForkInsertLocation.java
@@ -1,4 +1,32 @@
 package Array;
 
 public class _35_simple_ForkInsertLocation {
+    public static void main(String[] args){
+        int[] nums = {1, 3, 5, 6};
+        int[] nums1 = {1,3};
+        int target = 4;
+        Solution35 s = new Solution35();
+        int x = s.searchInsert(nums, target);
+        System.out.print(x);
+    }
+}
+class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        if(target < nums[0]){
+            return 0;
+        }else if(target > nums[nums.length-1]){
+            return nums.length;
+        }else return mid(nums, 0, nums.length-1, target);
+    }
+    public int mid(int[] nums, int start, int finish, int target){
+        int loc = start;
+        if(start == finish){
+            loc = finish;
+        }else if(nums[(start + finish)/2] >= target){
+            loc = mid(nums, start, (start + finish)/2, target);
+        }else if(nums[(start + finish)/2] < target){
+            loc = mid(nums, (start + finish)/2 + 1, finish, target);
+        }
+        return loc;
+    }
 }
