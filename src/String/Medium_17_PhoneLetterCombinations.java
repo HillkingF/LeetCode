3c824c3604b7069d97aa14a5793237773d3b4987
HillkingF <976129707@qq.com>
2020-08-18 18:47:40 +0800

    6

---
+++ src/String/Medium_17_PhoneLetterCombinations.java
@@ -0,0 +1,17 @@
+package String;
+
+import java.util.List;
+
+/**
+ * 回溯算法：递归求解。下一层的条件都满足时加入组合，不满足时回溯并改变上一层的记录
+ */
+public class Medium_17_PhoneLetterCombinations {
+    public List<String> letterCombinations(String digits) {
+
+    }
+    public String findRecurrent(String last, String next){
+
+
+    }
+
+}
--- src/String/Medium_6_Zconvert.java
+++ src/String/Medium_6_Zconvert.java
@@ -9,75 +9,33 @@ public class Medium_6_Zconvert {
 
     }
     public static String convert(String s, int numRows) {
-        if(s.length() <= numRows || numRows == 1) return s;
         String[][] stemp = new String[numRows][1];
-//        for(int c = 0; c < numRows; c++){
-//            stemp[c][0] = "";
-//        }
-        int layers = s.length()/(2*numRows - 2);
-        // 最后一层先待定
-        int sign = 0;
-        if(s.length() % (2*numRows - 2) != 0){
-            sign = 1;
-            layers++;
-        }
-
-        int rows;
-        int i = 1;
-        int j = 0;
-        int dirc = 0;
-        for(i = 1; i <= layers; i++){
+        if(s.length() <= numRows || numRows == 1) return s;
+        for(int c = 0; c < numRows; c++) stemp[c][0] = "";
+        int onepart = 2*numRows - 2;
+        int yushu = s.length() % onepart == 0 ? 0 : 1;
+        int layers = s.length()/(onepart) + yushu;
+        int rows, dirc, end;
+        int num;
+        for(int i = 1; i <= layers; i++){
             rows = 0;
-            if(sign == 1 && i == layers){
-                dirc = 0;
-                for ( j = (2*numRows - 2) * (i - 1); j < s.length(); j++){
-                    if(rows == 0){
-                        stemp[0][0] = stemp[0][0] + s.substring(j, j+1);
-                        rows++;
-                    }else if(rows == numRows){
-                        rows = rows - 2;
-                        stemp[rows][0] += s.substring(j, j + 1);
-                        dirc = 1;
-                    }else {
-                        if(dirc == 1){
-                            rows--;
-                            stemp[rows][0] += s.substring(j, j + 1);
-                        }else {
-                            stemp[rows][0] += s.substring(j, j + 1);
-                            rows ++;
-                        }
-                    }
-                } // for 2
-            }else{
-                dirc = 0;
-                for (j = (2*numRows - 2) * (i - 1); j < (2*numRows - 2) * i; j++){
-                    if(j == (2*numRows - 2) * (i - 1)){
-                        stemp[0][0] += s.substring(j, j+1);
-                        rows++;
-                    }else if(rows == numRows){
-                        dirc = 1;
-                        rows = rows - 2;
-                        stemp[rows][0] += s.substring(j, j + 1);
-                        rows--;
-                    }else if(rows > 0 && rows < numRows){
-                        stemp[rows][0] += s.substring(j, j + 1);
-                        if(dirc == 1){
-                            rows--;
-                        }else{
-                            rows++;
-                        }
-
-                    }
-                } // for 2
-            }
-
+            dirc = 0;
+            num = onepart * (i - 1);
+            if(yushu == 1 && i == layers)end = s.length();
+            else end = num + onepart;
+            for (int j = num; j < end; j++){
+                if(rows == numRows){
+                    dirc = 1;
+                    stemp[rows-2][0] += s.substring(j, j + 1);
+                    rows = rows - 3;
+                }else{
+                    if(dirc == 1)stemp[rows--][0] += s.substring(j, j + 1);
+                    else stemp[rows++][0] += s.substring(j, j + 1);
+                }
+            } // for 2
         }// for 1
-
-        String str = "";
-        for(int k = 0; k < numRows; k++ ){
-            str += stemp[k][0];
-        }
-        return str;
+        for(int k = 1; k < numRows; k++ )stemp[0][0] += stemp[k][0];
+        return stemp[0][0];
 
     }
 }
