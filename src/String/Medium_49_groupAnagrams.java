529c756156c971b2066c556a5dc30b33d3db00e7
HillkingF <976129707@qq.com>
2020-08-24 23:54:40 +0800

    49

--- src/String/Medium_49_groupAnagrams.java
+++ src/String/Medium_49_groupAnagrams.java
@@ -16,41 +16,27 @@ public class Medium_49_groupAnagrams {
         char[] arr = strs[0].toCharArray();
         Arrays.sort(arr);
         String dictsort = new String(arr);
-
-        char[] arr2;
         String bijiao;
-
         HashMap<String, List<String>> hashMap = new HashMap<String,List<String> >();
         List<String> xiang = new ArrayList<>();
         xiang.add(strs[0]);
         hashMap.put(dictsort, xiang);
-        System.out.println(hashMap.get(dictsort));
-
         for(int i = 1; i < strs.length; i++){
-            System.out.println(hashMap.get(dictsort));
-            arr2 = strs[i].toCharArray();
-            Arrays.sort(arr2);
-            bijiao = new String(arr2);
-            System.out.println(bijiao);
-
+            arr = strs[i].toCharArray();
+            Arrays.sort(arr);
+            bijiao = new String(arr);
             if(hashMap.containsKey(bijiao)){
                 xiang = hashMap.get(bijiao);
-                System.out.println(xiang);
                 xiang.add(strs[i]);
-
                 hashMap.put(bijiao, xiang);
             }else{
-                xiang.clear();
-                xiang.add(strs[i]);
-                hashMap.put(bijiao, xiang);
+                List<String> l = new ArrayList<>();
+                l.add(strs[i]);
+                hashMap.put(bijiao,l);
             }
         }
-
         List<List<String>> list = new ArrayList<>();
-        for(int km = 0; km < hashMap.size(); km++)
-            list.add(hashMap.get(km));
-
+        for (String key:hashMap.keySet()) list.add(hashMap.get(key));
         return list;
-
     }
 }
