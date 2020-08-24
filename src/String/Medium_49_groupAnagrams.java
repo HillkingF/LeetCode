2a80944f2df3b867df40ee5db15e0d0e365cdae6
HillkingF <976129707@qq.com>
2020-08-24 21:51:54 +0800

    49

--- src/String/Medium_49_groupAnagrams.java
+++ src/String/Medium_49_groupAnagrams.java
@@ -1,29 +1,56 @@
 package String;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Medium_49_groupAnagrams {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<String> dict = new ArrayList<>();
+    public static void main(String[] args){
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> l = groupAnagrams(strs);
 
-        dict.add(strs[0]);
-        boolean sign = true;
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        char[] arr = strs[0].toCharArray();
+        Arrays.sort(arr);
+        String dictsort = new String(arr);
+
+        char[] arr2;
+        String bijiao;
+
+        HashMap<String, List<String>> hashMap = new HashMap<String,List<String> >();
+        List<String> xiang = new ArrayList<>();
+        xiang.add(strs[0]);
+        hashMap.put(dictsort, xiang);
+        System.out.println(hashMap.get(dictsort));
+
         for(int i = 1; i < strs.length; i++){
-            for(int k = 0; k < dict.size(); k++){
-                for(int j = 0; j < strs[i].length(); j++){
-                    if(dict.get(k).contains(strs[i].substring(j, j+1))){
-//                      sign = true;
-                    }else{sign = false;}
-                }
-                if(sign != false){
+            System.out.println(hashMap.get(dictsort));
+            arr2 = strs[i].toCharArray();
+            Arrays.sort(arr2);
+            bijiao = new String(arr2);
+            System.out.println(bijiao);
 
-                }
+            if(hashMap.containsKey(bijiao)){
+                xiang = hashMap.get(bijiao);
+                System.out.println(xiang);
+                xiang.add(strs[i]);
 
+                hashMap.put(bijiao, xiang);
+            }else{
+                xiang.clear();
+                xiang.add(strs[i]);
+                hashMap.put(bijiao, xiang);
             }
-
-
         }
+
+        List<List<String>> list = new ArrayList<>();
+        for(int km = 0; km < hashMap.size(); km++)
+            list.add(hashMap.get(km));
+
+        return list;
 
     }
 }
