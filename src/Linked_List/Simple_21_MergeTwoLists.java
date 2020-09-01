99c6a9221382d189dcda06c97dbf927382f99a1c
HillkingF <976129707@qq.com>
2020-09-01 16:32:21 +0800

    21

--- src/Linked_List/Simple_21_MergeTwoLists.java
+++ src/Linked_List/Simple_21_MergeTwoLists.java
@@ -1,7 +1,50 @@
 package Linked_List;
+class ListNode {
+    int val;
+    ListNode next; // 指向下一个链表节点
+    ListNode() {}  // 链表节点类的构造方法
+    ListNode(int val) { this.val = val; }  // 链表节点类的构造方法
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; } // 链表节点类的构造方法
+}
 
 public class Simple_21_MergeTwoLists {
     public static void main(String[] args){
+        ListNode l1 = null, l1last = null, l2 = null;
+
+        ListNode temp = new ListNode();
+        temp.val = 1;
+        temp.next = null;
+
+        // 创建l1;
+        l1 = temp;
+        l1last = l1;
+
+        temp.val = 2;
+        temp.next = null;
+        l1last.next = temp;
+
+        temp.val = 4;
+        temp.next = null;
+        l1last.next = temp;
+
+//        // 创建l2
+//        temp.val = 1;
+//        temp.next = null;
+//        l2 = temp;
+//        temp.val = 3;
+//        temp.next = null;
+//        l2.next = temp;
+//        temp.val = 4;
+//        temp.next = null;
+//        l2.next.next = temp;
+
+        temp.val = l1.val;
+        temp.next = l1.next;
+        while(temp != null){
+            System.out.print(temp.val);
+            temp = temp.next;
+        }
+
 
     }
 
@@ -9,42 +52,22 @@ public class Simple_21_MergeTwoLists {
      * 【定义链表结构】
      * 其实就是定义一个链表节点类，其中规定了如何存储数据，如何记录位置
      */
-    public static class ListNode {
-        int val;
-        ListNode next; // 指向下一个链表节点
-        ListNode() {}  // 链表节点类的构造方法
-        ListNode(int val) { this.val = val; }  // 链表节点类的构造方法
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; } // 链表节点类的构造方法
-    }
+
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) { //l1,l2分别是头结点
-        ListNode l = null;
-        ListNode p=null, q= null;
-        if(l1.next == null && l2.next != null){
-            l = l2;
-        }else if(l2.next == null && l1.next != null){
-            l = l1;
-        }else if(l1.next == null && l2.next == null){
-
-        }else{ //两个都不为空
-            while(l1.next != null && l2.next != null){
-                if(l1.val <= l2.val){
-                    if(l == null){//说明是头结点开始
-                        l.val = l1.val;
-                        l.next = null;
-                        p = l1.next;
-                    }else{
-
-                    }
-                }else if(l1.val > l2.val){
-                    if(l == null){//说明是头结点开始
-                        l.val = l2.val;
-                        l.next = null;
-                    }
-                }
+        ListNode l = new ListNode(-1);  // 头节点
+        ListNode prev = l;   //尾结点
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                prev.next = l1;
+                l1 = l1.next;
+            }else{
+                prev.next = l2;
+                l2 = l2.next;
             }
+            prev = prev.next;  //=null
         }
-
-        return l;
+        prev.next = l1 == null ? l2 : l1;  // prev.val 和 prev.next只表示prev指针上的单元名字，next不表示下一个位置
+        return l.next;
     }
 
 }
---
+++ src/Linked_List/Simple_83_DeleteDuplicates.java
@@ -0,0 +1,12 @@
+package Linked_List;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+public class Simple_83_DeleteDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+
+    }
+}
