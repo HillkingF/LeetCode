97a4118aead815d9a202f09a5e9ba620a1933fe5
HillkingF <976129707@qq.com>
2020-09-01 17:12:42 +0800

    83

--- src/Linked_List/Simple_83_DeleteDuplicates.java
+++ src/Linked_List/Simple_83_DeleteDuplicates.java
@@ -1,12 +1,24 @@
 package Linked_List;
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
+//class ListNode {
+//    int val;
+//    ListNode next;
+//    ListNode(int x) { val = x; }
+//}
 public class Simple_83_DeleteDuplicates {
     public ListNode deleteDuplicates(ListNode head) {
+        //需要优化的地方是：清除无用的指针
+        ListNode p = head;
+        while(p != null && p.next != null){
+            System.out.println(p.val);
+            if(p.val == p.next.val){
+                p.next = p.next.next;
+            }else{
+                p = p.next;
+            }
+        }
+        return head;
+
 
     }
 }
