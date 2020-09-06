d74c97bc3d50eb614927dc1f6f41773100d3c70e
HillkingF <976129707@qq.com>
2020-09-06 11:07:19 +0800

    206

--- src/Linked_List/Simple_206_reverseList.java
+++ src/Linked_List/Simple_206_reverseList.java
@@ -5,24 +5,15 @@ import java.util.List;
 public class Simple_206_reverseList {
     public ListNode reverseList(ListNode head) {
         if(head == null) return null;
-        ListNode l =  new ListNode();
-        ListNode temp = new ListNode();
-        ListNode lh = head;
-
-        // 初始化
-        temp.val = lh.val;
-        temp.next = null;
-        l.val = 0;
-        l.next = temp;
-        while(lh.next != null){
-            lh = lh.next;
-            temp.next = null;
-            temp.val = lh.val;
-            temp.next = l.next;
-            l.next = null;
-            l.next = temp;
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode temp = cur.next;   //将待操作的后续节点存储到temp中
+            cur.next = pre;  //将带操作的节点的指针域指向前面的节点，这样带操作的节点就和后面的节点断开了
+            pre = cur;   //pre指针指向刚刚加入的节点
+            cur = temp;  //cur指向的这个节点操作完毕，重新指向temp中的节点，迭代操作
         }
-        return l;
+        return pre;
 
 
     }
