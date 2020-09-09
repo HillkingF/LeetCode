a8fd07fd753d4f1ed90a826e0d3f5b79959f8e3a
HillkingF <976129707@qq.com>
2020-09-09 20:38:00 +0800

    24-new

--- src/Linked_List/Medium_24_swapPairs.java
+++ src/Linked_List/Medium_24_swapPairs.java
@@ -3,13 +3,20 @@ package Linked_List;
 public class Medium_24_swapPairs {
     public ListNode swapPairs(ListNode head) {
         int count = 0;
+
+
         ListNode last = new ListNode(0);
         last.next = head;
 
         ListNode last1 = last;
         ListNode p = last.next;
+
+
         ListNode temp = new ListNode(0);
         temp.next = null;
+
+
+
         ListNode after = new ListNode(0);
         after.next = null;
         while(p != null){
@@ -32,5 +39,33 @@ public class Medium_24_swapPairs {
             }
         }
         return last1;
+    }
+
+    public ListNode swapPairs1(ListNode head) {   //优化
+        int count = 0;
+
+        ListNode newhead = new ListNode(0);
+        newhead.next = head;
+        ListNode last = newhead;
+
+        ListNode temp = new ListNode();
+        ListNode after = new ListNode();
+        while(head != null){
+            if(count == 0){
+                count = 1;
+                temp = head;
+                head = head.next;
+            }else{
+                count = 0;
+                after = head.next;
+                temp.next = after;
+                head.next = temp;
+                last.next = head;
+
+                last = temp;
+                head = after;
+            }
+        }
+        return newhead.next;
     }
 }
