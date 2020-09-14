6cab7d88740c82eedad259ce265cf4ad74d772a8
HillkingF <976129707@qq.com>
2020-09-14 19:22:43 +0800

    138

--- src/Linked_List/Medium_138_copyRandomList.java
+++ src/Linked_List/Medium_138_copyRandomList.java
@@ -1,7 +1,24 @@
 package Linked_List;
 
+/**
+ * 理解深拷贝和浅拷贝的区别
+ * 浅拷贝：改了新的链表地址，旧的链表也改变
+ * 深拷贝：新建一个链表，只复制值，不对旧的链表造成影响
+ */
 public class Medium_138_copyRandomList {
     public Node copyRandomList(Node head) {
 
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
     }
 }
