59f65a74df2e7fd412eae4a05a532987ce265a80
HillkingF <976129707@qq.com>
2020-09-29 18:48:20 +0800

    1441

--- src/Stack/Simple_1441_buildArray.java
+++ src/Stack/Simple_1441_buildArray.java
@@ -1,7 +1,26 @@
 package Stack;
 
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Simple_1441_buildArray {
     public List<String> buildArray(int[] target, int n) {
-
+        List<String> res = new ArrayList<>();
+        int index = 0;
+        for(int i = 1; i<= n; i++){
+            if(i > target[target.length-1])break;
+            else{
+                if(target[index] > i){
+                    res.add("Push");
+                    res.add("Pop");
+                }else if(target[index] == i){
+                    res.add("Push");
+                    index ++;
+                }
+            }
+        }
+        return res;
     }
 }
