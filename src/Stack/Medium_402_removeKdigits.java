8b485e215bc3b3536c9cd0bc6c2bde48f865479c
HillkingF <976129707@qq.com>
2020-10-21 20:53:40 +0800

    402

--- src/Stack/Medium_402_removeKdigits.java
+++ src/Stack/Medium_402_removeKdigits.java
@@ -1,6 +1,66 @@
 package Stack;
 
+import java.util.Stack;
+
 public class Medium_402_removeKdigits {
-    public String removeKdigits(String num, int k) {
+    public static void main(String[] args){
+        String num = "1432219";
+        int k = 3;
+        String res = removeKdigits(num, k);
+        System.out.println(res);
+
+    }
+    public static String removeKdigits(String num, int k) {
+        String res = "";
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+        int strnow = 0;
+        int sign = 1;
+        int i = 0;
+        for (i = 0; i < num.length(); i++){
+            if(stack.isEmpty())
+                if(num.charAt(i)!='0')stack.push(Integer.valueOf(num.charAt(i)));
+            else{
+                strnow = Integer.valueOf(num.charAt(i));
+                if(strnow != 0) {
+                    if (strnow > stack.peek()) {
+                        count += 1;
+                    } else if (strnow == stack.peek()) stack.push(Integer.valueOf(num.charAt(i)));
+                    else {
+                        while (!stack.isEmpty()) {
+                            if (count < k) {
+                                stack.pop();
+                                count += 1;
+                            } else{
+                                sign = 1;
+                                break;
+                            }
+                        }
+                    }
+                }else{  //此时字符为0
+                    while(!stack.isEmpty()){
+                        if (count < k) {
+                            stack.pop();
+                            count += 1;
+                        } else{
+                            sign = 1;
+                            break;
+                        }
+                    }
+                }
+                if( sign == 0) break;
+
+                }
+        }
+        while(!stack.isEmpty()){
+            res = stack.pop().toString() + res;
+        }
+        if(i == num.length()){
+
+        }else{
+            res = res + num.substring(i);
+        }
+        return res;
+
     }
 }
