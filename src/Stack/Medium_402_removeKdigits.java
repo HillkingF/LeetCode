dc780d6d09ffe5838ce975504d7a5a978555f803
HillkingF <976129707@qq.com>
2020-10-26 21:10:04 +0800

    402

--- src/Stack/Medium_402_removeKdigits.java
+++ src/Stack/Medium_402_removeKdigits.java
@@ -4,74 +4,60 @@ import java.util.Stack;
 
 public class Medium_402_removeKdigits {
     public static void main(String[] args){
-        String num = "5347";
-        int k = 2;
+        String num = "10";
+        int k = 1;
         String res = removeKdigits(num, k);
         System.out.println(res);
 
     }
 
     /**
-     *先获得一个排序的字符串哈希表
+     *思路：
+     * 从左到右遍历
+     * 每一个字符跟栈顶比较，如果栈顶大，则出栈（删除），否则入栈
+     * 如果从某个时刻单调递增了，则直接从后面删除依次删除即可
      */
     public static String removeKdigits(String num, int k) {
-        String res = "";
-        Stack<Integer> stack = new Stack<>();
+        Stack<Character> stack = new Stack();
         int count = 0;
-        int strnow = 0;
-        int sign = 0;
-        int i = 0;
-        if(num.length() == k) return "0";
-        for (i = 0; i < num.length(); i++){
-            strnow = Integer.valueOf(num.substring(i,i+1));
-            System.out.println(count);
-            if(stack.isEmpty()){
-                if(num.charAt(i)!='0'){
-                    stack.push(strnow);
-                }
-            }else{
-                if(strnow != 0) {
-                    if(count >= k) break;
-                    if (strnow > stack.peek()) {
+        for(int i = 0; i < num.length(); i++){
+            if(count < k){
+                while(!stack.isEmpty()) {
+                    if (stack.peek() > num.charAt(i) && count < k) {
+                        stack.pop();
                         count++;
-                    } else if (strnow == stack.peek()) {
-                        stack.push(strnow);
                     } else {
-                        while (!stack.isEmpty()) {
-                            if (count < k) {
-                                stack.pop();
-                                count ++;
-                            } else{
-                                sign = 1;
-                                break;
-                            }
-                        }
-                    }
-                }else{  //此时字符为0
-                    while(!stack.isEmpty()){
-                        if (count < k) {
-                            stack.pop();
-                            count ++;
-                        } else{
-                            sign = 1;
-                            break;
-                        }
+                        break;
                     }
                 }
-                if( sign == 1) break;
+                if(stack.isEmpty() && num.charAt(i) == '0'){
+
+                }else{
+                    stack.push(num.charAt(i));
 
                 }
+
+            }else{
+                if(stack.isEmpty() && num.charAt(i) == '0'){
+
+                }else{
+                    stack.push(num.charAt(i));
+                }
+
+            }
         }
+        String res = "";
+        if(stack.isEmpty()) res = "0";
         while(!stack.isEmpty()){
             res = stack.pop().toString() + res;
         }
-        if(i == num.length()){
-
-        }else{
-            res = res + num.substring(i);
-        }
-        if(res.equals("")){
-            res = "0";
+        if(count < k){
+            int cha = k-count;
+            if(res.length() > cha){
+                res = res.substring(0, res.length()-cha);
+            }else{
+                res = "0";
+            }
         }
         return res;
 
