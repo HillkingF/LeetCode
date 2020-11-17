e5ffc9253cc0e46a07949235d05087db2899311c
HillkingF <976129707@qq.com>
2020-11-17 14:58:07 +0800

    856

--- src/Stack/Medium_856_scoreOfParentheses.java
+++ src/Stack/Medium_856_scoreOfParentheses.java
@@ -2,6 +2,15 @@ package Stack;
 
 public class Medium_856_scoreOfParentheses {
     public int scoreOfParentheses(String S) {
+        /** 题目
+         * () 得 1 分。  设为 A 情况，栈不空，则A入栈
+         * AB 得 A + B 分，其中 A 和 B 是平衡括号字符串。   设为
+         * (A) 得 2 * A 分，其中 A 是平衡括号字符串。
+         */
+
+        /**
+         * 一组（）用A来表示
+         */
 
     }
 }
