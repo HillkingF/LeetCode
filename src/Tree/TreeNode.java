478024c1dfb036133c9c41af3f6cfd13984d1afe
HillkingF <976129707@qq.com>
2020-11-24 18:29:11 +0800

    chushihua

--- src/Tree/TreeNode.java
+++ src/Tree/TreeNode.java
@@ -17,20 +17,20 @@ public class TreeNode {  // 定义一棵二叉树
 }
 
 class pre_definetree{
-    Stack<TreeNode> stack = new Stack<>();
     public static void main(String[] args){
         int[] nums = {1, 2, 3, -1, 4, -1,-1, -1, 6, -1, 7, 8, 9};
+        TreeNode t = def(nums);
     }
     // 前序初始化一棵树
-    public TreeNode def(int[] nums){
-
+    public static TreeNode def(int[] nums){
+        Stack<TreeNode> stack = new Stack<>();
         TreeNode T = new TreeNode();
         TreeNode root = T;
         if(nums.length == 0) {
             return T;
         }
 
-        for(int i=0; i<= nums.length; i++){
+        for(int i=0; i< nums.length; i++){
             if(i == 0){
                 T.val = nums[0];
                 T.right = null;
--- src/Tree/walk_tree.java
+++ src/Tree/walk_tree.java
@@ -1,12 +1,15 @@
 package Tree;
 
 
+import java.util.List;
+
 public class walk_tree {
     public static void main(String[] args){
 
     }
     // 前序遍历
     public List<Integer> preorderTraversal(TreeNode root) {
+        return null;
 
     }
 }
