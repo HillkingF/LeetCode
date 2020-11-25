b4a616edcfb7154aa538d95128915633dd920396
HillkingF <976129707@qq.com>
2020-11-25 21:13:04 +0800

    101

--- src/Tree/Simple_101_isSymmetric.java
+++ src/Tree/Simple_101_isSymmetric.java
@@ -38,6 +38,45 @@ public class Simple_101_isSymmetric {
         }
     }
 
-    // 迭代的写法怎么写呢？？
-    
+    /**
+     *迭代的写法:（迭代就是一次次循环，递归是调用方法自己）
+     * 创建一个队列是从递归变成迭代的常用手段
+     * 思路：
+     * 首先将根节点两次入队。下面开始循环：
+     * 每一轮出队两次，先出队的给p，后出队的给q；
+     * p的左子树和p的右子树比较；p的右子树和q的左子树比较：
+     * 先比较p和q两个点的值，不相同直接返回false；相同继续下面的步骤：
+     * 当p和q左右分别有子树的时候同时入队；当p和q只有其中一个有子树则返回false；当两个都分别没有左右子树则返回while的下一轮；
+     * 直到最后队列空，说明都对称，返回true
+     */
+    public static boolean iter(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) return true;
+        queue.add(root);
+        queue.add(root);
+        TreeNode p = new TreeNode();
+        TreeNode q = new TreeNode();
+
+        while(!queue.isEmpty()){
+            p = queue.poll();
+            q = queue.poll();
+            if(p.val != q.val){
+                return false;
+            }
+
+            if(p.left != null && q.right != null){
+                queue.add(p.left);
+                queue.add(q.right);
+            }
+            if(p.right != null && q.left != null){
+                queue.add(p.right);
+                queue.add(q.left);
+            }
+            if((p.left!=null&&q.right==null)|| (p.left==null && q.right!= null)){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
