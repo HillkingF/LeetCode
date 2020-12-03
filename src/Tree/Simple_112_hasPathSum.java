7ddffccc4adb157fce33b1bb4abb584cbcae51a0
HillkingF <976129707@qq.com>
2020-12-03 18:38:57 +0800

    112

--- src/Tree/Simple_112_hasPathSum.java
+++ src/Tree/Simple_112_hasPathSum.java
@@ -1,8 +1,22 @@
 package Tree;
 
+import javafx.scene.layout.BorderPane;
+
+import java.util.Stack;
+
 public class Simple_112_hasPathSum {
+
+    /**
+     * 递归的思想简单，代码少，体会一下 或 的用法
+     */
     public boolean hasPathSum(TreeNode root, int sum) {
-        
+        if(root == null) return false;
+        if(root.left == null && root.right == null)
+            return root.val == sum;
+
+        Boolean leftres = hasPathSum(root.left, sum - root.val);
+        Boolean rightres = hasPathSum(root.right, sum - root.val);
+        return leftres || rightres;
 
     }
 }
