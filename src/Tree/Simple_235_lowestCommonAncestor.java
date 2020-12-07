f87732d9ce70cb34f598d064b7a3167be01063c7
HillkingF <976129707@qq.com>
2020-12-07 11:13:24 +0800

    235

---
+++ src/Tree/Simple_235_lowestCommonAncestor.java
@@ -0,0 +1,15 @@
+package Tree;
+
+public class Simple_235_lowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int l = p.val > q.val ? p.val : q.val;
+        int s = p.val > q.val ? q.val : p.val;
+        TreeNode res = root;
+
+        if(s <= root.val && root.val <= l) res = root;
+        else if(l < root.val) res = lowestCommonAncestor(root.left, p, q);
+        else if(s < root.val) res = lowestCommonAncestor(root.right, p, q);
+        return res;
+
+    }
+}
