cfc79a9aab84c281b9592e0222153ef172b81ed0
HillkingF <976129707@qq.com>
2020-12-08 17:53:50 +0800

    999

---
+++ src/Tree/Medium_98_isValidBST.java
@@ -0,0 +1,7 @@
+package Tree;
+
+public class Medium_98_isValidBST {
+    public boolean isValidBST(TreeNode root) {
+
+    }
+}
---
+++ src/Tree/Simple_96_numTrees.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class Simple_96_numTrees {
+    // 这道题跟95题类似，统计可以构造的二叉搜索树的个数
+    // 可以直接调用95题的结果然后返回列表个数
+    // 官方的两种方法：一个是动态规划，一个是数学公式。做到动态规划的时候再看看
+    public int numTrees(int n) {
+
+        return 0;
+
+
+    }
+}
