ea32849812cd98969b8524ae4882ff63033a83a9
HillkingF <976129707@qq.com>
2020-12-08 18:44:17 +0800

    98

--- src/Tree/Medium_98_isValidBST.java
+++ src/Tree/Medium_98_isValidBST.java
@@ -2,6 +2,30 @@ package Tree;
 
 public class Medium_98_isValidBST {
     public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+
+        Boolean rightsub = isValidBST(root.right);
+        Boolean leftsub = isValidBST(root.left);
+        if(rightsub == false || leftsub == false) return false;
+        else{
+            if(root.right != null){
+                TreeNode righttree = root.right;
+                while(righttree.left != null){
+                    righttree = righttree.left;
+                }
+                if(righttree.val <= root.val) return false;
+            }
+            if(root.left != null){
+                TreeNode lefttree = root.left;
+                while(lefttree.right != null){
+                    lefttree = lefttree.right;
+                }
+                if(lefttree.val >= root.val) return false;
+            }
+            return true;
+
+        }
 
     }
+
 }
