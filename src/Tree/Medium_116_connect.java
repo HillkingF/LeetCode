c526f6c73c0c1b9ac18df9b3503399cf1d282c5e
HillkingF <976129707@qq.com>
2020-12-11 17:16:47 +0800

    116

--- src/Tree/Medium_116_connect.java
+++ src/Tree/Medium_116_connect.java
@@ -1,7 +1,35 @@
 package Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Medium_116_connect {
-    public Node connect(Node root) {
+    public Node connect1(Node root) {
+        // 第一种方法：使用层次遍历，根据每一层的节点个数来填充
+        Queue<Node> queue = new LinkedList<>();
+        int size;
+        if(root==null) return null;
+
+        queue.add(root);
+        while(!queue.isEmpty()){
+            size = queue.size();
+            while(size > 0){
+                Node tmp = queue.poll();
+                if(tmp.left != null){
+                    queue.add(tmp.left);
+                    queue.add(tmp.right);
+                }
+                tmp.next = size == 1 ? null : queue.peek();
+                size --;
+            }
+        }
+        return root;
+    }
+
+    public Node connect(Node root){
+        //第二种方法：递归，常量级空间
+        
 
     }
+
 }
