442677f893af6f0a79a20bcd0ada5d1cc3f6c29c
HillkingF <976129707@qq.com>
2020-12-31 17:53:35 +0800

    96

---
+++ src/DP/Ti/Medium_96_numTrees.java
@@ -0,0 +1,13 @@
+package DP.Ti;
+
+public class Medium_96_numTrees {
+    public int numTrees(int n) {
+        /**
+         * 【知识点】
+         * 二叉搜索树：任意一个点的左子树值<根<右子树
+         * 【思路】
+         * 1、最后一步：
+         */
+
+    }
+}
