06820221a7075dd0ab65d422bca4b7fd29c0a6d7
HillkingF <976129707@qq.com>
2021-03-03 21:17:10 +0800

    76

--- src/Hot_100/Hard_76_minWindow.java
+++ src/Hot_100/Hard_76_minWindow.java
@@ -7,13 +7,12 @@ import java.util.stream.StreamSupport;
 
 public class Hard_76_minWindow {
     public static void main(String[] args){
-        String s = "asabeiwafgc";
-        String t = "ac";
-        // String res = minWindow(s,t);
-        String res = minsort(s);
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        String res = minWindow1(s,t);
+        // String res = minsort(s);
         System.out.println(res);
     }
-
 
     /**
      * 下面可运行，但是太慢了，超出leetcode 时间限制
@@ -66,29 +65,65 @@ public class Hard_76_minWindow {
     public static String minWindow1(String s, String t) {
         if(s.length() < t.length()) return "";
         if(s.equals(t)) return s;
-
         int start = 0;
         int end = 0;
-        String x = minsort(t);
         String res = "";
         int reslength = Integer.MAX_VALUE;
-        while(end < s.length()){
-            String y = minsort(s.substring(start, end + 1));
+        int tmplen = 0;
+        int sign = 0;
+
+        // 构造字符数量字典
+        HashMap<Character, Integer> smap = new HashMap<>();
+        HashMap<Character, Integer> tmap = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            if(tmap.containsKey(t.charAt(i))){ tmap.put(t.charAt(i), tmap.get(t.charAt(i)) + 1);
+            }else{ tmap.put(t.charAt(i), 1); }
+            smap.put(t.charAt(i), 0);
         }
+        if(tmap.containsKey(s.charAt(0))){  smap.put(s.charAt(0), 1); }
 
 
+        while(end < s.length() && start <= end){
+            sign = 0;
+            String y = s.substring(start, end + 1);  // start 到 end范围内的子串
+            System.out.println(y);
+            tmplen = y.length();
+            // 对比目前维护的smap中是否包含全部的tmap
+            for(char a : tmap.keySet()){
+                if(smap.get(a) >= tmap.get(a)){
+                }else{ // 说明y 不包含t
+                    sign = 1;
+                    end ++;
+                    if(end < s.length() && tmap.containsKey(s.charAt(end))){
+                        smap.put(s.charAt(end), smap.get(s.charAt(end)) + 1);
+                    }
+                    break;
+                }
+            }
+            if(sign == 0){ // 说明y包含t
+                if(tmplen < reslength){
+                    reslength = tmplen;
+                    res = y;
+                }
+                if(smap.containsKey(s.charAt(start))){
+                    smap.put(s.charAt(start), smap.get(s.charAt(start)) - 1);
+                    start ++;
+                }else{
+                    while(!tmap.containsKey(s.charAt(start))){
+                        if(start < end){
+                            start ++;
+                        }else{
+                            break;
+                        }
 
-        return "";
-
-        
-
-
+                    }
+                }
+                if(start > end){
+                    end ++;
+                }
+            }
+        }
+        return res;
     }
-    // 对子序列进行排序
-    public static String minsort(String x){
-        char[] xchar = x.toCharArray();
-        Arrays.sort(xchar);
-        String newstr = new String(xchar);
-        return newstr;
-    }
+
 }
