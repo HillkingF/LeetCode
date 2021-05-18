7e0e142a87bedae3dfada72e4ea18fc5ddb491e5
HillkingF <976129707@qq.com>
2021-05-18 20:22:08 +0800

    148

--- src/Hot_100/Medium_148_sortList.java
+++ src/Hot_100/Medium_148_sortList.java
@@ -1,6 +1,8 @@
 package Hot_100;
 
 
+import com.sun.security.jgss.GSSUtil;
+
 /**
  * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
  * 进阶：
@@ -21,6 +23,19 @@ public class Medium_148_sortList {
      * sortList1方法：
      * 使用的是冒泡法，超出了时间限制
      */
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4, null);
+        head.next = new ListNode(2, null);
+        head.next.next = new ListNode(1, null);
+        head.next.next.next = new ListNode(3, null);
+        // head.next.next.next.next = new ListNode(0, null);
+        ListNode res = sortList2(head);
+        while(res != null){
+            System.out.println(res.val);
+            res = res.next;
+        }
+
+    }
     public ListNode sortList1(ListNode head){
         if(head == null || head.next == null) return head;
         // 当链表至少有两个节点时，执行下面的操作
@@ -66,6 +81,171 @@ public class Medium_148_sortList {
         }
         return newhead.next;
     }
+
+
+    /* 采用自底向上的归并排序实现最快，空间最少
+    *  自底向上可以使用循环（反之使用递归）
+    * */
+    public static ListNode sortList2(ListNode head){
+        // 1.首先统计链表长度
+        int len = 0;
+        ListNode tmp = head;
+        while(tmp != null){
+            len ++;
+            tmp = tmp.next;
+        }
+
+        // 2.定义变量：归并区间、
+        int sublen = 1; // 归并区间（一半）
+        int start = 0;  // 每一轮从第0个节点开始，直到最后一个节点
+        int count = 0;  // 计算每一对儿中，放到的节点个数
+
+        ListNode newhead = new ListNode(0,null);  // 用于保存每一轮归并后的部分有序链表的头结点
+        ListNode newlast = newhead; // 新链表的尾结点
+
+        // 3.最外层循环用于控制归并的长度sublen(每轮sublen和sublen个两两归并)：每次sublen以2倍的方式增加
+        for(sublen = 1; sublen < len; sublen = sublen * 2){
+            newhead.next = null;
+            newlast = newhead;
+            ListNode head0 = new ListNode(0, head);
+
+            // 4.第二层循环用于两对儿两对儿归并
+            for(start = 0; start < len; start=start+2*sublen){ // start = 0, 2, 4 。。。, 所以第一轮比较的是0-1个节点
+                // 遍历找到这一对儿节点的中间节点，分别变成A和B组，后面的始终由head指针指向
+                count = start;  // 每一对儿中都从start位置开始计数
+                // 每一对儿中的左右两部分A和B
+                ListNode A = head0;  // head0中是比原来的head链表多出来的一个头结点。主要是为了便于记录访问结果
+                ListNode Alast = A;
+                ListNode B = head0;
+                ListNode Blast = B;
+                // 寻找并记录每一对儿无序区间中的A和B部分
+                while(count < start + 2*sublen && head != null){
+                    if(count <= start + sublen - 1){  // 找A
+                        if(count == start){
+                            A = head;
+                            Alast = A;
+                            head = head.next;
+                        }else{
+                            Alast.next = head;
+                            head = head.next;
+                            Alast = Alast.next;
+                        }
+                        Alast.next = null;
+                        count ++;
+                    }
+                    else{
+                        if(count == start + sublen){
+                            B = head;
+                            Blast = B;
+                            head = head.next;
+                        }else{
+                            Blast.next = head;
+                            head = head.next;
+                            Blast = Blast.next;
+                        }
+                        Blast.next = null;
+                        count ++;
+                    }
+                }
+                // 如果最后一个区间内只有A部分，A已经是有序的，所以直接增加到新链表末位即可
+                if(count <= start + sublen){ // 说明只有A，直接添加到新数组
+                    newlast.next = A;
+                }else{ // 否则如果A，B都存在，则将A和B两个有序数组合并成
+                    while(A != null && B != null){
+                        if(A.val <= B.val){
+                            newlast.next = A;
+                            A = A.next;
+                            newlast = newlast.next;
+                            newlast.next = null;
+                        }else{
+                            newlast.next = B;
+                            B = B.next;
+                            newlast = newlast.next;
+                            newlast.next = null;
+                        }
+                    }
+                    if(A==null && B == null){
+                    }else if(A == null && B != null){
+                        newlast.next = B;
+                    }else if(A != null&& B== null){
+                        newlast.next = A;
+                    }
+                }
+                while(newlast.next != null){
+                    newlast = newlast.next;
+                }
+            } // for
+            head = newhead.next;
+        }
+        return head;
+
+
+
+
+
+    }
+
+
+    public static ListNode sortList(ListNode head) {
+        if (head == null) {return head;}
+        // 1. 统计链表的节点个数
+        int length = 0;
+        ListNode node = head;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        ListNode dummyHead = new ListNode(0, head);
+        for (int subLength = 1; subLength < length; subLength=subLength * 2) {
+            ListNode prev = dummyHead, curr = dummyHead.next;
+            while (curr != null) {
+                ListNode head1 = curr;
+                for (int i = 1; i < subLength && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode head2 = curr.next;
+                curr.next = null;
+                curr = head2;
+                for (int i = 1; i < subLength && curr != null && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode next = null;
+                if (curr != null) {
+                    next = curr.next;
+                    curr.next = null;
+                }
+                ListNode merged = merge(head1, head2);
+                prev.next = merged;
+                while (prev.next != null) {
+                    prev = prev.next;
+                }
+                curr = next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+    public static ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+
 }
 
 
