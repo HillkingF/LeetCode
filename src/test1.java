a3bf965365e9087e853a7ff6a2d7b1644ce6b633
Hillking <976129707@qq.com>
2021-07-01 19:06:14 +0800

    111

--- src/test1.java
+++ src/test1.java
@@ -5,5 +5,7 @@ public class test1 {
         // 1.测试一下,使用有参构造创建列表数组,且容量为0时的容量大小. 结果注释在源码44行
         ArrayList arrayList = new ArrayList(0);
         System.out.println(arrayList.size());
+
+        Thread
     }
 }
